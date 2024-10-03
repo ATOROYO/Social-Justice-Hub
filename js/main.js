518fa209d42bf1aa84e31c95e2df74198a62f89c
ATOROYO <davidatoroyosika@gmail.com>
2024-10-03 19:33:04 +0300

    Debuged

--- js/main.js
+++ js/main.js
@@ -1,9 +1,17 @@
 // Select the menu icon and nav links
-const menuIcon = document.getElementById("menu-icon");
-const navLinks = document.getElementById("nav-links");
+// const menuIcon = document.getElementById("menu-icon");
+// const navLinks = document.getElementById("nav-links");
 
-// Add event listener to the menu icon
-menuIcon.addEventListener("click", () => {
-  // Toggle the 'active' class to show or hide the navigation links
+// // Add event listener to the menu icon
+// menuIcon.addEventListener("click", () => {
+//   // Toggle the 'active' class to show or hide the navigation links
+//   navLinks.classList.toggle("active");
+// });
+
+function toggleMenu() {
+  const menuIcon = document.querySelector(".menu-icon");
+  const navLinks = document.querySelector(".nav-links");
+
+  menuIcon.classList.toggle("active");
   navLinks.classList.toggle("active");
-});
+}
